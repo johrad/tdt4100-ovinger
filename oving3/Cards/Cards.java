f6fb6b39b99b442721daf46ebecc3788e0a0fa62
jpdragic <jpdragic@gmail.com>
2023-02-07 17:08:27 +0100

    from sql

---
+++ oving3/Cards/Cards.java
@@ -0,0 +1,9 @@
+package oving3.Cards;
+
+// Great video: https://youtu.be/aj9N5dQXFpE?t=154 
+
+public class Cards {
+    public static void main(String[] args) {
+        System.out.println("cock");
+    }
+}
